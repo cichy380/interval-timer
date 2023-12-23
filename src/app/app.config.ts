bdea0233fa962b54f49360af064781aae7c75560
cichy380 <marcin.dobroszek@gmail.com>
2023-12-23 13:22:52 +0100

    Add PWA
    ng add @angular/pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { APP_ROUTES } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(APP_ROUTES)
-  ]
+    provideRouter(APP_ROUTES),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
